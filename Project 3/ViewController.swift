aab95d694db7bd76d43215252d354165164ac343
Tracy Hainer <trhainer@tdesign.us>
2019-07-16 19:04:19 -0400

    Initial Commit

---
+++ Project 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 3
+//
+//  Created by Tracy Hainer on 7/16/19.
+//  Copyright © 2019 Tracy Hainer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
